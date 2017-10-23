aa380b4ccaf04150de18e22c65ecc3f57024b745
Jussi Jartamo <jussi.jartamo@gofore.com>
2017-10-23 13:23:53 +0300

    test fixtures

--- src/ulkoiset_rajapinnat/config.clj
+++ src/ulkoiset_rajapinnat/config.clj
@@ -4,16 +4,16 @@
             [clojure.string :as str]
             [environ.core :refer [env]]))
 
-(def ^:private property-key :ulkoiset-rajapinnat-properties)
+(def ulkoiset-rajapinnat-property-key :ulkoiset-rajapinnat-properties)
 
 (defn ^:private read-config-path-from-args [args]
   (first (filter some?
                  (map (fn [arg]
-                        (if (str/starts-with? arg (name property-key))
-                          (subs arg (+ 1 (count (name property-key)))) nil)) args))))
+                        (if (str/starts-with? arg (name ulkoiset-rajapinnat-property-key))
+                          (subs arg (+ 1 (count (name ulkoiset-rajapinnat-property-key)))) nil)) args))))
 
 (defn ^:private read-config-path-from-env []
-  (env property-key))
+  (env ulkoiset-rajapinnat-property-key))
 
 (defn ^:private read-args-or-env
   [args]
@@ -25,7 +25,7 @@
       (do
         (log/info "Using config property {} from env.vars!" from-env)
         from-env)
-      (throw (Exception. (str (name property-key) " is mandatory! Either give it in args or set env.var!"))))))
+      (throw (Exception. (str (name ulkoiset-rajapinnat-property-key) " is mandatory! Either give it in args or set env.var!"))))))
 
 (defn read-configuration-file-first-from-varargs-then-from-env-vars
   "Reads configuration file. File path&name is searched from varargs and env.variables. Key is 'ulkoiset-rajapinnat-properties'."
--- test/ulkoiset_rajapinnat/core_test.clj
+++ test/ulkoiset_rajapinnat/core_test.clj
@@ -1,8 +1,18 @@
 (ns ulkoiset-rajapinnat.core-test
   (:require [clojure.test :refer :all]
             [clj-log4j2.core :as log]
+            [ulkoiset-rajapinnat.config :refer :all]
             [ulkoiset-rajapinnat.core :refer :all]))
+
+(defn fixture [f]
+  (let [server-handle (start-server [(str (name ulkoiset-rajapinnat-property-key) "=" "test.edn")])]
+    (f)
+    (-> (meta server-handle)
+        :server
+        (.stop 100))))
+
+(use-fixtures :once fixture)
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0))))
