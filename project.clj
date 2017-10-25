c04d6ac0e7bdba43e03a4f970e678269a6775390
Jussi Jartamo <jussi.jartamo@gofore.com>
2017-10-25 14:03:39 +0300

    uberjar config

--- project.clj
+++ project.clj
@@ -3,6 +3,11 @@
   :url "http://api.opintopolku.fi"
   :license {:name "EUPL"
             :url "http://www.osor.eu/eupl/"}
+  :deploy-repositories {"snapshots" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"}
+                        "releases" {:url "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"}}
+  :repositories [["oph-releases" "https://artifactory.oph.ware.fi/artifactory/oph-sade-release-local"]
+                 ["oph-snapshots" "https://artifactory.oph.ware.fi/artifactory/oph-sade-snapshot-local"]
+                 ["ext-snapshots" "https://artifactory.oph.ware.fi/artifactory/ext-snapshot-local"]]
   :dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                  [org.clojure/clojure "1.8.0"]
                  [environ "1.1.0"]
@@ -18,14 +23,19 @@
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.9.0"]
                  [clj-log4j2 "0.2.0"]
                  ]
-
+  :prep-tasks ["compile"]
   :jvm-opts ["-Dlog4j.configurationFile=test/log4j2.properties"
              "-Dulkoiset-rajapinnat-properties=test.edn"
              "-XX:+TieredCompilation" "-XX:TieredStopAtLevel=1"]
-
+  :uberjar-name "ulkoiset-rajapinnat-0.1.0-SNAPSHOT-standalone.jar"
   :main ulkoiset-rajapinnat.core
+  :aot [ulkoiset-rajapinnat.core]
 
-  :profiles {:plugins [[lein-ring "0.11.0"]]
+  :plugins [[lein-resource "14.10.2"]
+            [lein-deploy-artifacts "0.1.0"]]
+
+  :profiles {:uberjar {:prep-tasks ["compile" "resource"]}
+             :plugins [[lein-ring "0.11.0"]]
              :dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [tempfile "0.2.0"]
                                   [clj-http "3.7.0"]
