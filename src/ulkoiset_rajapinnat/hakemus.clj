67a49bdb0514b8ac9187bbf5132031a8e3ef7a64
Jussi Jartamo <jussi.jartamo@gofore.com>
2017-11-10 16:54:14 +0200

    almost all hakemus data

--- src/ulkoiset_rajapinnat/hakemus.clj
+++ src/ulkoiset_rajapinnat/hakemus.clj
@@ -10,34 +10,60 @@
 
 (comment
   ; missing fields
-  pohjakoulutus_2aste
-  pohjakoulutus_kk
   lahtokoulun_organisaatio_oid
   lahtokoulun_kuntakoodi
-  harkinnanvarainen_valinta
-  ensikertalaisuus
-  hakijan_koulusivistyskieli
-  ulkomailla_suoritetun_toisen_asteen_tutkinnon_suoritusmaa)
+  ensikertalaisuus)
+
+(defn collect-preference-keys-by-sija [document]
+  (let [ht (get-in document ["answers" "hakutoiveet"])
+        preference-keys (filter not-empty (map #(re-matches #"preference(\d)-(.*)" %) (keys ht)))]
+    (group-by second preference-keys)))
+
+(defn find-ends-with [s endswith]
+  (first (filter #(str/ends-with? % endswith) s)))
+
+(defn convert-hakutoive [document preferences]
+  (let [hakutoiveet (get-in document ["answers" "hakutoiveet"])
+        pref-keys (set (map first (.getValue preferences)))
+        get-endswith #(get hakutoiveet (find-ends-with pref-keys %))]
+    {:hakukohde_oid (get-endswith "-Koulutus-id")
+     :harkinnanvarainen_valinta (get-endswith "-discretionary-follow-up")
+     :sija (.getKey preferences)}))
 
 (defn hakutoiveet-from-hakemus [document]
-  (let [preference "preference"
-        koulutus-id "-Koulutus-id"
-        hakutoiveet (filter #(.endsWith (.getKey %) koulutus-id) (seq (get-in document ["answers" "hakutoiveet"])))
-        p-to-k (fn [key] (Integer/parseInt (.replace (.replace key preference "") koulutus-id "")))
-        converted-hakutoiveet (map (fn [a] {:sija (p-to-k (.getKey a)) :oid (.getValue a)}) hakutoiveet)]
-    {:hakutoiveet converted-hakutoiveet}))
+  (let [pref-keys-by-sija (collect-preference-keys-by-sija document)]
+    {:hakutoiveet (map (partial convert-hakutoive document) pref-keys-by-sija)}))
 
-(defn write-hakemus [count document]
+(defn koulutustausta-from-hakemus [pohjakoulutuskkodw document]
+  (let [koulutustausta (get-in document ["answers" "koulutustausta"])
+        koulusivistyskieli (remove nil? [(get koulutustausta "lukion_kieli")
+                                         (get koulutustausta "perusopetuksen_kieli")])
+        pohjakoulutus_2aste (get koulutustausta "POHJAKOULUTUS")
+        pohjakoulutus_kk (first (filter #(contains? koulutustausta %) pohjakoulutuskkodw))
+        ulkomailla_suoritetun_toisen_asteen_tutkinnon_suoritusmaa (get koulutustausta "pohjakoulutus_ulk_suoritusmaa")]
+    {:hakijan_koulusivistyskieli (first koulusivistyskieli)
+     :pohjakoulutus_2aste pohjakoulutus_2aste
+     :pohjakoulutus_kk pohjakoulutus_kk
+     :ulkomailla_suoritetun_toisen_asteen_tutkinnon_suoritusmaa ulkomailla_suoritetun_toisen_asteen_tutkinnon_suoritusmaa}))
+
+(defn remove-nils
+  [m]
+  (let [f (fn [[k v]] (when v [k v]))]
+    (clojure.walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
+
+(defn write-hakemus [pohjakoulutuskkodw count document]
   (let [json (to-json
-               (merge
+               (remove-nils (merge
                  (hakutoiveet-from-hakemus document)
+                 (koulutustausta-from-hakemus pohjakoulutuskkodw document)
                  {"hakemus_oid" (get document "oid")
                   "henkilo_oid" (get document "personOid")
                   "haku_oid" (get document "applicationSystemId")
-                  "hakemus_tila" (get document "state")}))]
+                  "hakemus_tila" (get document "state")})))]
     (if (= count 1) json (str "," json))))
 
-(defn fetch-hakemukset-for-haku [haku-oid mongo-client channel]
+(defn fetch-hakemukset-for-haku
+  [pohjakoulutuskkodw-promise haku-oid mongo-client channel]
   (let [is-headers-written (atom false)
         count (atom 0)
         database (.getDatabase mongo-client "hakulomake")
@@ -57,8 +83,9 @@
                                   (do
                                     (status channel 200)
                                     (body channel "[")))
-                                (-> channel
-                                    (body (write-hakemus (swap! count inc) document))))
+                                (let-flow [pohjakoulutuskkodw pohjakoulutuskkodw-promise]
+                                          (-> channel
+                                              (body (write-hakemus pohjakoulutuskkodw (swap! count inc) document)))))
                                ([_ throwable]
                                 (log/error "Failed to fetch stuff!" throwable)
                                 (close channel))))))))
@@ -66,5 +93,7 @@
 (defn hakemus-resource [config mongo-client haku-oid request]
   (with-channel request channel
                 (on-close channel (fn [status] (log/debug "Channel closed!" status)))
-                (fetch-hakemukset-for-haku haku-oid mongo-client channel)
+                (let [host-virkailija (config :host-virkailija)
+                      pohjakoulutuskkodw-promise (chain (fetch-koodisto host-virkailija "pohjakoulutuskkodw") #(vals %))]
+                  (fetch-hakemukset-for-haku pohjakoulutuskkodw-promise haku-oid mongo-client channel))
                 (schedule-task (* 1000 60 60) (close channel))))
