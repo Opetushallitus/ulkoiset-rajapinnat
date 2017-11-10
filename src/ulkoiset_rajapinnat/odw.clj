6bb0623a17240cec32c0c2e2ead14d4d1af67976
Jessika Penttinen <jessika.penttinen@siili.com>
2017-11-10 09:11:11 +0200

    http-kit odw-rajapintaan

--- src/ulkoiset_rajapinnat/odw.clj
+++ src/ulkoiset_rajapinnat/odw.clj
@@ -1,9 +1,15 @@
 (ns ulkoiset-rajapinnat.odw
-  (:require [ulkoiset-rajapinnat.rest :refer [parse-json-request]]
+  (:require [ulkoiset-rajapinnat.rest :refer [parse-json-request status body-and-close to-json]]
+            [org.httpkit.server :refer :all]
+            [org.httpkit.timer :refer :all]
             [clj-log4j2.core :as log]))
 
 (defn hakukohde-json [oid]
   {:hakukohdeOid oid})
 
 (defn odw-resource [config request]
-  (map hakukohde-json (parse-json-request request)))
+  (with-channel request channel
+    (on-close channel (fn [status] (log/debug "Channel closed!" status)))
+    (status channel 200)
+    (body-and-close channel (to-json (map hakukohde-json (parse-json-request request))))
+    (schedule-task (* 1000 60 60) (close channel))))
