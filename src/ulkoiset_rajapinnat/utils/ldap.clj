4e15721a7e678c565780517fa307bec624559d26
Jussi Jartamo <jussi.jartamo@gofore.com>
2017-12-14 08:36:19 +0200

    cas and ldap validator

---
+++ src/ulkoiset_rajapinnat/utils/ldap.clj
@@ -0,0 +1,29 @@
+(ns ulkoiset-rajapinnat.utils.ldap
+  (:require [clojure.string :as str])
+  (:import (com.unboundid.ldap.sdk LDAPConnection SearchRequest SearchScope Filter)))
+
+(defn fetch-user-from-ldap [config username]
+  (let [uid-filter (Filter/createEqualityFilter "uid" username)
+        search-request (SearchRequest. (-> config :ldap :basedn)
+                                       (SearchScope/SUB)
+                                       uid-filter
+                                       (into-array String
+                                                   ["description"
+                                                    "uid"
+                                                    "sn"
+                                                    "givenName"
+                                                    "employeeNumber"]))
+        connection (LDAPConnection.)]
+    (try
+      (.connect connection (-> config :ldap :host) (-> config :ldap :port))
+      (.bind connection (-> config :ldap :binddn) (-> config :ldap :password))
+      (let [result (.search connection search-request)
+            entry (-> result
+                      (.getSearchEntries)
+                      (.iterator)
+                      (.next))]
+        {:username username
+         :roles (.getValue (.getAttribute entry "description"))
+         :personOid (.getValue (.getAttribute entry "employeeNumber"))})
+      (finally (.close connection)))))
+
