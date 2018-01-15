bf9542877aa617a1260b5347304d22516616ada4
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-01-15 14:00:39 +0200

    node example simplified with bluebird

--- node_example.js
+++ node_example.js
@@ -1,23 +1,37 @@
 const http = require('http')
 const fs = require('fs')
-const querystring = require('querystring')
 const request = require('request')
 const cheerio = require('cheerio')
+const Promise = require('bluebird')
 
 const [file_with_creds_and_host] = process.argv.slice(-1)
 const [username, password, hostname] = fs.readFileSync(file_with_creds_and_host, 'utf8').split(/\r?\n/)
 
-request.post(hostname + '/cas/v1/tickets', {form:{username:username, password:password}}, (error,response,htmlPageWithTicket) => {
-	$ = cheerio.load(htmlPageWithTicket)
-	const tgt = $('form').attr('action')
-
-	request.post(tgt, {form:{service:hostname+'/ulkoiset-rajapinnat'}},(error,response,serviceticket) => {
-		const url = hostname + '/ulkoiset-rajapinnat/api/haku-for-year/2001?ticket=' + serviceticket
-		console.log('Calling URL ' + url)
-		request.get(url, (e,r,body) => {
-			console.log(body)
+const postForm = (url, form) => {
+	return new Promise((resolve, reject) => {
+		request.post(url, {form:form}, (error, response, body) => {
+			if(error) {
+				reject(error)
+			} else {
+				resolve(body)
+			}
 		})
 	})
-})
+}
 
+const ticketGrantingTicketFromResponseHtml = (htmlPageWithTicket) => {
+	$ = cheerio.load(htmlPageWithTicket)
+	const tgt = $('form').attr('action')
+	return tgt
+}
 
+postForm(hostname + '/cas/v1/tickets', {username:username, password:password})
+	.then(ticketGrantingTicketFromResponseHtml)
+	.then((tgtUrl) => postForm(tgtUrl,{service:hostname+'/ulkoiset-rajapinnat'}))
+	.then((serviceTicket) => {
+		const url = hostname + '/ulkoiset-rajapinnat/api/haku-for-year/2001?ticket=' + serviceTicket
+                console.log('Calling URL ' + url)
+		request.get(url, (e,r,body) => {
+                        console.log(body)
+                })		
+	})
