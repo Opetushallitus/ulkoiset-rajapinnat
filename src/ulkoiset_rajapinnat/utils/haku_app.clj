0f91a749c2e9505aca02cb7bae970d035e7aab0b
Jussi Jartamo <jussi.jartamo@gofore.com>
2018-01-25 09:57:22 +0200

    partition-all replaced due to memory problems. lazy seq didn't release already channelled data

--- src/ulkoiset_rajapinnat/utils/haku_app.clj
+++ src/ulkoiset_rajapinnat/utils/haku_app.clj
@@ -7,7 +7,9 @@
             [ulkoiset-rajapinnat.utils.cas :refer [service-ticket-channel]]
             [clojure.core.async :refer [chan promise-chan >! go put! close! alts! timeout <!]]
             [clojure.tools.logging :as log]
-            [clojure.core.async :as async]))
+            [clojure.core.async :as async])
+  (:import (com.fasterxml.jackson.core JsonFactory JsonToken)
+           (com.fasterxml.jackson.databind ObjectMapper)))
 
 (def haku-streaming-api "/haku-app/streaming/applications/listfull")
 (defn- hakemukset-for-hakukohde-oids-query [haku-oid hakukohde-oids] {"searchTerms" ""
@@ -27,30 +29,36 @@
          query (hakemukset-for-hakukohde-oids-query haku-oid hakukohde-oids)
          service-ticket-channel (service-ticket-channel host service username password)
          channel (chan 1)]
-     (go-try
+     (go
        (let [st (<? service-ticket-channel)
              response (client/post (str host haku-streaming-api) {:headers {"CasSecurityTicket" st
                                                                             "Content-Type"      "application/json"}
                                                                   :as      :stream
                                                                   :body    (to-json query)})
              body-stream (response :body)
-             lazy-hakemus-seq (parse-stream (clojure.java.io/reader body-stream))
-             ]
+             mapper (ObjectMapper.)
+             parser (-> (doto (JsonFactory.)
+                          (.setCodec mapper))
+                        (.createParser (clojure.java.io/reader body-stream)))]
          (try
-           (doseq [hakemus-batch (partition-all batch-size lazy-hakemus-seq)]
-             (if-let [error (some #(find % "error") hakemus-batch)]
-               (throw (RuntimeException. (str error)))
-               (if (not (>! channel hakemus-batch))
-                 (throw (RuntimeException. "Client disconnected! Releasing resources!")))))
-           (comment
-             "Somehow this doesnt work! Memory is not released!"
-             (if (not (alts! [[channel hakemus-batch]
-                              (timeout (* 30 60 1000))]))
-               (throw
-                 (RuntimeException.
-                   (str
-                     "Client disconnected or nobody read a batch from hakemus stream in 30 minutes! "
-                     "Closing connection to haku-app to release resources!")))))
+           (case (.nextToken parser)
+             (JsonToken/START_ARRAY))
+           (let [batch (java.util.ArrayList. batch-size)
+                 drain-to-vector (fn []
+                                   (let [v (vec (.toArray batch))]
+                                     (.clear batch)
+                                     (if (not-empty v)
+                                       v
+                                       nil)))]
+             (while (= (.nextToken parser) (JsonToken/START_OBJECT))
+               (let [obj (-> mapper
+                             (.readValue parser java.util.HashMap))]
+                 (-> batch (.add obj))
+                 (if (= (count batch) batch-size)
+                   (if (not (>! channel (drain-to-vector)))
+                     (throw (RuntimeException. "Client disconnected! Releasing resources!"))))))
+             (when-let [last-batch (drain-to-vector)]
+               (>! channel last-batch)))
            (catch Exception e
              (do
                (log/error "Failed to read hakemus json from 'haku-app'!" (.getMessage e))
