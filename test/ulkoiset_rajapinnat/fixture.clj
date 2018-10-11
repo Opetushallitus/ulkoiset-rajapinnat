987590e15556fc7c6914e0292c621d380c5b1877
oph-petar <petar@phz.fi>
2018-10-11 15:29:23 +0300

    Custom executor for tests

--- test/ulkoiset_rajapinnat/fixture.clj
+++ test/ulkoiset_rajapinnat/fixture.clj
@@ -1,12 +1,15 @@
 (ns ulkoiset-rajapinnat.fixture
   (:require [clojure.test :refer :all]
             [clojure.string :as str]
+            [clojure.core.async.impl.protocols :as protocols]
+            [clojure.core.async.impl.concurrent :as conc]
+            [clojure.core.async.impl.exec.threadpool :as tp]
             [ulkoiset-rajapinnat.freeport :refer :all]
             [ulkoiset-rajapinnat.generate-edn :refer :all]
             [ulkoiset-rajapinnat.utils.config :refer :all]
             [ulkoiset-rajapinnat.freeport :refer :all]
-            [ulkoiset-rajapinnat.core :refer :all]
-            ))
+            [ulkoiset-rajapinnat.core :refer :all])
+  (:import java.util.concurrent.Executors))
 
 (def fake-user {:personOid "1.2.246.562.24.1234567890"})
 
@@ -25,7 +28,18 @@
   (let [tmp-test-edn-file (data-to-tmp-edn-file test-configs)]
     (str (name ulkoiset-rajapinnat-property-key) "=" tmp-test-edn-file)))
 
+(defonce my-executor
+         (let [executor-svc (Executors/newFixedThreadPool
+                              1
+                              (conc/counted-thread-factory "async-dispatch-%d" true))]
+           (reify protocols/Executor
+             (protocols/exec [this r]
+               (.execute executor-svc ^Runnable r)))))
+
+
 (defn fixture [f]
   (let [server-close-handle (start-server [(test-edn)])]
+    (alter-var-root #'clojure.core.async.impl.dispatch/executor
+                    (constantly (delay my-executor)))
     (f)
     (server-close-handle)))
