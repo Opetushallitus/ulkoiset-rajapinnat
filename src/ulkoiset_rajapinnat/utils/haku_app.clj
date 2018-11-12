d9cab4d75dbbacb628d1fdcad0e802d58d3dec1a
Timo Rantalaiho <timo.rantalaiho@reaktor.com>
2018-11-12 10:10:52 +0200

    Delete unused code
    No functional changes

--- src/ulkoiset_rajapinnat/utils/haku_app.clj
+++ src/ulkoiset_rajapinnat/utils/haku_app.clj
@@ -1,18 +1,14 @@
 (ns ulkoiset-rajapinnat.utils.haku_app
-  (:require [clojure.string :as str]
-            [full.async :refer :all]
+  (:require [full.async :refer :all]
             [clj-http.client :as client]
             [cheshire.core :refer :all]
             [clojure.core.async :refer [chan promise-chan >! go put! close! alts! timeout <!]]
             [clojure.tools.logging :as log]
-            [clojure.core.async :as async]
             [ulkoiset-rajapinnat.utils.url-helper :refer [resolve-url]]
             [ulkoiset-rajapinnat.utils.read_stream :refer [read-json-stream-to-channel]]
             [ulkoiset-rajapinnat.utils.rest :refer [to-json]]
             [ulkoiset-rajapinnat.utils.cas :refer [fetch-service-ticket-channel]]
-            )
-  (:import (com.fasterxml.jackson.core JsonFactory JsonToken)
-           (com.fasterxml.jackson.databind ObjectMapper)))
+            ))
 
 (defn- hakemukset-for-hakukohde-oids-query [haku-oid hakukohde-oids] {"searchTerms" ""
                                                                       "asIds"       [haku-oid]
