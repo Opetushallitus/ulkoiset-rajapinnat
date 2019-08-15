67041c3148b597aac8fff7a34d11e15c72387b8e
teo.mertanen <teo.mertanen@gofore.com>
2019-08-15 11:11:46 +0300

    BUG-2044 more try-catching

--- src/ulkoiset_rajapinnat/utils/read_stream.clj
+++ src/ulkoiset_rajapinnat/utils/read_stream.clj
@@ -20,19 +20,31 @@
         (let [batch (java.util.ArrayList. batch-size)
               drain-to-vector (fn []
                                 (let [v (vec (.toArray batch))]
-                                  (.clear batch)
-                                  (when (not-empty v)
-                                    (log/info (format "sending value %s to reuslt-mapper" v))
-                                    (result-mapper v))))]
-          (while (= (.nextToken parser) (JsonToken/START_OBJECT))
-            (let [obj (-> mapper
-                          (.readValue parser java.util.HashMap))]
-              (-> batch (.add obj))
-              (if (= (count batch) batch-size)
-                (if (not (>! channel (drain-to-vector)))
-                  (throw (RuntimeException. "Channel was closed before reading stream completed!"))))))
-          (when-let [last-batch (drain-to-vector)]
-            (>! channel last-batch))))
+                                  (try
+                                    (.clear batch)
+                                    (when (not-empty v)
+                                      (log/info (format "sending value %s to reuslt-mapper" v))
+                                      (result-mapper v))
+                                    (catch Exception e
+                                      (log/error "Exception in drain-to-vector" e)
+                                      (throw e)))))]
+          (try
+            (while (= (.nextToken parser) (JsonToken/START_OBJECT))
+              (let [obj (-> mapper
+                            (.readValue parser java.util.HashMap))]
+                (-> batch (.add obj))
+                (if (= (count batch) batch-size)
+                  (if (not (>! channel (drain-to-vector)))
+                    (throw (RuntimeException. "Channel was closed before reading stream completed!"))))))
+            (catch Exception e
+              (log/error "Exception in read-stream while loop" e)
+              (throw e)))
+          (try
+            (when-let [last-batch (drain-to-vector)]
+              (>! channel last-batch))
+            (catch Exception e
+              (log/error "Exception in read-stream when-let" e)
+              (throw e)))))
       (catch Exception e
         (log/error "Exception when reading stream" e)
         (>! channel e))
