405323426943fdbf4d17e3d06bd9578c0165692b
Mikko Siukola <mikko.siukola@cybercom.com>
2023-05-22 10:39:08 +0300

    OY-4274 Cleanup

--- src/kotlin/ulkoiset_rajapinnat/ataru/dto/Ataruhakemus.kt
+++ src/kotlin/ulkoiset_rajapinnat/ataru/dto/Ataruhakemus.kt
@@ -1,32 +1,5 @@
 package ulkoiset_rajapinnat.ataru.dto
 
-//[
-//  {
-//    "asuinmaa": "string",
-//    "pohjakoulutus_kk_ulk_country": "string",
-//    "hakukohde_oids": [
-//      "string"
-//    ],
-//    "pohjakoulutus_kk": [
-//      {
-//        "pohjakoulutuskklomake": "string",
-//        "suoritusvuosi": 0
-//      }
-//    ],
-//    "hakemus_oid": "string",
-//    "henkilo_oid": "string",
-//    "hakemus_tila": "string",
-//    "haku_oid": "string",
-//    "kotikunta": "string",
-//    "hakutoiveet": [
-//      {
-//        "hakukohde_oid": "string",
-//        "sija": 0
-//      }
-//    ]
-//  }
-//]
-
 data class HakemuksenHakutoive(
     val hakukohde_oid: String,
     val sija: Int,
--- src/kotlin/ulkoiset_rajapinnat/oppijanumerorekisteri/dto/OnrHenkilo.kt
+++ src/kotlin/ulkoiset_rajapinnat/oppijanumerorekisteri/dto/OnrHenkilo.kt
@@ -1,56 +1,5 @@
 package ulkoiset_rajapinnat.oppijanumerorekisteri.dto
 
-//[
-//  {
-//    "oidHenkilo": "1.2.246.562.24.76784922593",
-//    "hetu": "090914A9134",
-//    "kaikkiHetut": [],
-//    "passivoitu": false,
-//    "etunimet": "Samuel Testi",
-//    "kutsumanimi": "Samuel",
-//    "sukunimi": "Mäkynen-Testi",
-//    "aidinkieli": {
-//      "kieliKoodi": "fi",
-//      "kieliTyyppi": "suomi"
-//    },
-//    "asiointiKieli": null,
-//    "kansalaisuus": [
-//      {
-//        "kansalaisuusKoodi": "246"
-//      }
-//    ],
-//    "kasittelijaOid": "1.2.246.562.24.66631583590",
-//    "syntymaaika": "2014-09-09",
-//    "sukupuoli": "2",
-//    "kotikunta": null,
-//    "oppijanumero": "1.2.246.562.24.76784922593",
-//    "turvakielto": false,
-//    "eiSuomalaistaHetua": false,
-//    "yksiloity": false,
-//    "yksiloityVTJ": true,
-//    "yksilointiYritetty": true,
-//    "duplicate": false,
-//    "created": 1411326275578,
-//    "modified": 1584058500126,
-//    "vtjsynced": null,
-//    "yhteystiedotRyhma": [],
-//    "yksilointivirheet": [],
-//    "passinumerot": [],
-//    "kielisyys": [],
-//    "henkiloTyyppi": "OPPIJA"
-//  }
-//]
-
-//{:yksiloity              (get henkilo "yksiloity")
-////       :henkilotunnus          (get henkilo "hetu")
-////       :syntyma_aika           (get henkilo "syntymaaika")
-////       :etunimet               (get henkilo "etunimet")
-////       :sukunimi               (get henkilo "sukunimi")
-////       :sukupuoli_koodi        (get henkilo "sukupuoli")
-////       :aidinkieli             (get henkilo "aidinkieli")
-////       :hakijan_kansalaisuudet (mapv fetch-maakoodi-from-koodisto-cache (map #(get % "kansalaisuusKoodi") kansalaisuusKoodit))})
-////    {}))
-
 data class Kansalaisuus(
     val kansalaisuusKoodi: String
 )
